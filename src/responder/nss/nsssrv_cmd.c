b4abe4088ceec0189f97b1a0e3fce37c23066206
Stephen Gallagher <sgallagh@redhat.com>
2011-04-15 09:19:40 -0400

    Fix regressions in the negative cache
    Do not throw a DP error when failing to delete a nonexistent entity
    
    Add debug logging to the negative cache
    
    Fix a regression with the negative cache in multi-domain configurations
    
    Fix regression where nonexistent entries were never added to the negative cache

--- src/responder/nss/nsssrv_cmd.c
+++ src/responder/nss/nsssrv_cmd.c
@@ -532,12 +532,17 @@ static int nss_cmd_getpwnam_search(struct nss_dom_ctx *dctx)
 
         /* if neg cached, return we didn't find it */
         if (ret == EEXIST) {
-            DEBUG(2, ("User [%s] does not exist! (negative cache)\n", name));
+            DEBUG(2, ("User [%s] does not exist in [%s]! (negative cache)\n",
+                      name, dom->name));
             /* if a multidomain search, try with next */
             if (cmdctx->check_next) {
                 dom = dom->next;
                 continue;
             }
+            /* There are no further domains or this was a
+             * fully-qualified user request.
+             */
+            return ENOENT;
         }
 
         DEBUG(4, ("Requesting info for [%s@%s]\n", name, dom->name));
@@ -560,19 +565,19 @@ static int nss_cmd_getpwnam_search(struct nss_dom_ctx *dctx)
         }
 
         if (dctx->res->count == 0 && !dctx->check_provider) {
-            /* if a multidomain search, try with next */
-            if (cmdctx->check_next) {
-                dom = dom->next;
-                continue;
-            }
-
-            DEBUG(2, ("No results for getpwnam call\n"));
-
             /* set negative cache only if not result of cache check */
             ret = sss_ncache_set_user(nctx->ncache, false, dom->name, name);
             if (ret != EOK) {
                 return ret;
             }
+
+            /* if a multidomain search, try with next */
+            if (cmdctx->check_next) {
+                dom = dom->next;
+                if (dom) continue;
+            }
+
+            DEBUG(2, ("No results for getpwnam call\n"));
 
             return ENOENT;
         }
@@ -1794,12 +1799,17 @@ static int nss_cmd_getgrnam_search(struct nss_dom_ctx *dctx)
 
         /* if neg cached, return we didn't find it */
         if (ret == EEXIST) {
-            DEBUG(2, ("Group [%s] does not exist! (negative cache)\n", name));
+            DEBUG(2, ("Group [%s] does not exist in [%s]! (negative cache)\n",
+                    name, dom->name));
             /* if a multidomain search, try with next */
             if (cmdctx->check_next) {
                 dom = dom->next;
                 continue;
             }
+            /* There are no further domains or this was a
+             * fully-qualified user request.
+             */
+            return ENOENT;
         }
 
         DEBUG(4, ("Requesting info for [%s@%s]\n", name, dom->name));
@@ -1822,19 +1832,19 @@ static int nss_cmd_getgrnam_search(struct nss_dom_ctx *dctx)
         }
 
         if (dctx->res->count == 0 && !dctx->check_provider) {
-            /* if a multidomain search, try with next */
-            if (cmdctx->check_next) {
-                dom = dom->next;
-                continue;
-            }
-
-            DEBUG(2, ("No results for getgrnam call\n"));
-
             /* set negative cache only if not result of cache check */
             ret = sss_ncache_set_group(nctx->ncache, false, dom->name, name);
             if (ret != EOK) {
                 return ret;
             }
+
+            /* if a multidomain search, try with next */
+            if (cmdctx->check_next) {
+                dom = dom->next;
+                if (dom) continue;
+            }
+
+            DEBUG(2, ("No results for getgrnam call\n"));
 
             return ENOENT;
         }
@@ -2827,12 +2837,17 @@ static int nss_cmd_initgroups_search(struct nss_dom_ctx *dctx)
 
         /* if neg cached, return we didn't find it */
         if (ret == EEXIST) {
-            DEBUG(2, ("User [%s] does not exist! (negative cache)\n", name));
+            DEBUG(2, ("User [%s] does not exist in [%s]! (negative cache)\n",
+                      dom->name, name));
             /* if a multidomain search, try with next */
             if (cmdctx->check_next) {
                 dom = dom->next;
                 continue;
             }
+            /* There are no further domains or this was a
+             * fully-qualified user request.
+             */
+            return ENOENT;
         }
 
         DEBUG(4, ("Requesting info for [%s@%s]\n", name, dom->name));
@@ -2851,19 +2866,19 @@ static int nss_cmd_initgroups_search(struct nss_dom_ctx *dctx)
         }
 
         if (dctx->res->count == 0 && !dctx->check_provider) {
-            /* if a multidomain search, try with next */
-            if (cmdctx->check_next) {
-                dom = dom->next;
-                continue;
-            }
-
-            DEBUG(2, ("No results for initgroups call\n"));
-
             /* set negative cache only if not result of cache check */
             ret = sss_ncache_set_user(nctx->ncache, false, dom->name, name);
             if (ret != EOK) {
                 return ret;
             }
+
+            /* if a multidomain search, try with next */
+            if (cmdctx->check_next) {
+                dom = dom->next;
+                if (dom) continue;
+            }
+
+            DEBUG(2, ("No results for initgroups call\n"));
 
             return ENOENT;
         }
